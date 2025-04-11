69ff266a24164b1664fc61fc955447f25215c639
Rob Myers <1243316+robmyersrobmyers@users.noreply.github.com>
2025-04-11 03:40:55 -0400

    add memory usage statistics to reproducer.go

--- v1/topdown/main/reproducer.go
+++ v1/topdown/main/reproducer.go
@@ -11,6 +11,8 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"runtime"
+	"time"
 	_ "unsafe" // needed for go:linkname
 
 	"github.com/open-policy-agent/opa/v1/ast"
@@ -20,6 +22,9 @@ import (
 // Complex GQL schema to test with
 const gqlSchema = `https://docs.github.com/public/fpt/schema.docs.graphql`
 
+// Seconds between memory dump information
+const memDumpSeconds = 3
+
 //go:linkname  builtinGraphQLParseSchema github.com/open-policy-agent/opa/v1/topdown.builtinGraphQLParseSchema
 func builtinGraphQLParseSchema(topdown.BuiltinContext, []*ast.Term, func(*ast.Term) error) error
 
@@ -28,6 +33,21 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error downloading: %s\n", err)
 	}
+
+	// Dump the memory usage every few seconds
+	ticker := time.NewTicker(memDumpSeconds * time.Second)
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				PrintMemUsage()
+			case <-done:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
 
 	// This should allocate a bunch of memory and not return
 	fmt.Fprintf(os.Stderr, "Now passing %d bytes to builtinGraphQLParseSchema() to reproduce the issue in ast.InterfaceToValue()\n", len(b))
@@ -40,6 +60,9 @@ func main() {
 			return nil
 		},
 	)
+
+	// Stop printing memory usage
+	done <- true
 }
 
 func downloadSchema(url string) ([]byte, error) {
@@ -50,4 +73,21 @@ func downloadSchema(url string) ([]byte, error) {
 	}
 	defer response.Body.Close()
 	return io.ReadAll(response.Body)
+}
+
+// From: https://gist.github.com/j33ty/79e8b736141be19687f565ea4c6f4226
+// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
+// of garage collection cycles completed.
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
